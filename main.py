6887e34ce82c48a31dbcdb1ac19d00e7512898a6
GusaynRizovskiy <makkks200426013@mail.ru>
2023-10-21 21:26:23 +0300

    Change the autentification's procedure(using Regular Expression add right input data)

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import re
+from PyQt5.QtWidgets import QMessageBox
 def process(self,username,surname,login_window):
-    if username == "user" and surname == "user":
+    if re.fullmatch(r'[а-яА-яa-zA-Z]+',username) and re.fullmatch(r'[а-яА-яa-zA-Z]+',surname):
         print("correct")
         self.work_part()
+    else:
+        mess_box = QMessageBox()
+        mess_box.setWindowTitle("Предупреждение")
+        mess_box.setText("Некорректные данные")
+        mess_box.setInformativeText("Введите фамилию и имя правильно")
+        mess_box.setIcon(QMessageBox.Warning)
+        mess_box.setStandardButtons(QMessageBox.Ok|QMessageBox.Cancel)
+        mess_box.show()
+        mess_box.exec_()
+        print("incorrect")
